b5647e360b0ba59722af178db5beedbcd738ddf7
Jaci-Xavier <devxvr@gmail.com>
2023-08-30 22:44:15 -0300

    fix: importação na model

--- app/backend/src/database/migrations/20230827054215-table-matches.ts
+++ app/backend/src/database/migrations/20230827054215-table-matches.ts
@@ -1,5 +1,5 @@
 import { Model, QueryInterface, DataTypes } from 'sequelize';
-import { Match } from '../../Interfaces/Matches/Matches';
+import  Match  from '../../Interfaces/Matches/Matches';
 
 export default {
   up(queryInterface: QueryInterface) {
